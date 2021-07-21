4ea665121a2a72f70c1ba4d0f302fdbff86a7691
Andrey999 <gromovandrey1985@gmail.com>
2021-07-21 09:41:27 +0300

    create context

---
+++ src/context/ThemeProvider.tsx
@@ -0,0 +1,33 @@
+import React, { useContext, useState, useEffect } from 'react'
+import { setThemeCssVariable } from 'utils/setThemeCssVariables'
+import { setLocalStorage, getLocalStorage } from 'utils/localStorage'
+
+export const ThemeContext = React.createContext<any>(null)
+
+export const THEME_NEITRAL = 'neitral'
+export const THEME_DARK = 'dark'
+export const THEME_LIGHT = 'light'
+
+export const ThemeProvider: React.FC = ({ children, ...props }) => {
+    const [theme, setTheme] = useState<string | {}>({})
+
+    useEffect(() => {
+        const getThemeKey = getLocalStorage('theme')
+        setTheme(getThemeKey)
+        setThemeCssVariable(getThemeKey)
+    }, [])
+
+    const changeTheme = (themeName: string) => {
+        setLocalStorage('theme', themeName)
+        setTheme(themeName)
+        setThemeCssVariable(themeName)
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme, changeTheme }} {...props}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export const useTheme = () => useContext(ThemeContext)
