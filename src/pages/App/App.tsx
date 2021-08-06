6d6dd129a75ae0b0fba437937a2be71e427860e7
Andrey999 <gromovandrey1985@gmail.com>
2021-08-06 08:54:44 +0300

    update App

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { routes } from '../../routes/config'
+import { Header } from 'components/Header/Header'
 import './style.css'
 
 export const App = () => {
     return (
         <div className="wrapper">
-            Work
+            <BrowserRouter>
+                <Header />
+
+                <Switch>
+                    {routes.map((route: any, index: any) =>
+                        <Route
+                            key={index}
+                            path={route.path}
+                            exact={route.exact}
+                            component={route.component}
+                        />
+                    )}
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
