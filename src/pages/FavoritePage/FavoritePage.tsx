7a015dd5e31eb22c1595be601f1c51ffcfdc571d
Andrey999 <gromovandrey1985@gmail.com>
2021-08-07 17:13:38 +0300

    FavoritePage update

--- src/pages/FavoritePage/FavoritePage.tsx
+++ src/pages/FavoritePage/FavoritePage.tsx
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { PeopleList } from 'components/People/PeopleList'
+import { IPeopleList } from 'types/type'
 import './style.css'
+
+// type TypeName = ({ name: string; img: string } | string)[]
 
 export const FavoritePage = () => {
     const state = useSelector((state: any) => state.favoriteReducer)
-    const [people, setPeople] = useState([])
+    const [people, setPeople] = useState<IPeopleList[]>([])
 
     useEffect(() => {
         const arrPeople = Object.entries(state.favorite)
+
         if (arrPeople.length) {
-            const favoritePeople = arrPeople.map(item => {
+            const favoritePeople = arrPeople.map((item: any) => {
                 return {
                     id: item[0],
                     // ...item[1]
                     name: item[1].name,
                     image: item[1].img
                 }
-
             })
             setPeople(favoritePeople)
         }
@@ -26,8 +29,8 @@ export const FavoritePage = () => {
     return (
         <ul className="favorite__container">
             {people.length
-                ? people.map((item: any) => <PeopleList key={item.id} item={item} />)
-                : <h1 className="favoritePage__noData">No favorites</h1>
+                ? people.map(item => <PeopleList key={item.id} item={item} />)
+                : <h1 className="favoritePage__noData">Здесь будут любимые персонажи</h1>
             }
         </ul>
     )
